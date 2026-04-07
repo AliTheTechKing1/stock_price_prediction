4e0362553091f4b77db2a888c00bb5c6e975bdba
AliTheTechKing1 <alithetechking@gmail.com>
2026-04-07 22:50:33 +0300

    imported data

---
+++ src/fetch_data.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+import pandas as pd
+
+df = yf.download("AAPL", start="2018-01-01", end="2024-01-01")
+df = df[["Close"]]
+
+df = df.ffill()
+print(df.isnull().sum())
+
+df.to_csv("AAPL.csv")
